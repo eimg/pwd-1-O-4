52adf30fcf88171e8f27acb50c836137251e0887
Ei Maung <eimg@fairwayweb.com>
2026-02-21 16:50:11 +0630

    blog

---
+++ blog/resources/views/articles/detail.blade.php
@@ -0,0 +1,19 @@
+@extends("layouts.app")
+
+@section("content")
+    <div class="container" style="max-width: 600px">
+        <div class="card mb-2 bg-white border-primary">
+            <div class="card-body">
+                <h3 class="card-title">{{ $article->title }}</h3>
+                <div class="text-muted">
+                    {{ $article->created_at }}
+                </div>
+                <p>{{ $article->body }}</p>
+                <a href="{{ url("/articles/delete/$article->id") }}"
+                    class="btn btn-sm btn-outline-danger">
+                    Delete
+                </a>
+            </div>
+        </div>
+    </div>
+@endsection
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ArticleController;
 
-Route::get('/articles', function() {
-    return "Article List";
-});
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::get('/articles/detail/{id}', [ArticleController::class, 'detail']);
+Route::get('/articles/delete/{id}', [ArticleController::class, 'delete']);
 
-Route::get('/articles/detail/{id}', function ($id) {
-    return "Article Detail $id";
-});
+Route::get('/', [ArticleController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
